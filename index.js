1586ecc3f5308edefe6405adb093d8fa0975b45d
Tanin Rahman <taninr87@gmail.com>
2022-11-24 16:03:34 +0600

    add user in a database

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
 
 // middlewares
-app.use(cors);
+app.use(cors());
 app.use(express.json());
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_pass}@cluster0.flsgo3q.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+  try{
+      const userCollections = client.db('becheDaw').collection('users');
+      
+      app.post('/users', async(req, res) => {
+        const user = req.body;
+        const result = await userCollections.insertOne(user);
+        console.log(result);
+      })
+  }
+  finally{
+
+  }
+}
+run().catch(console.log);
 
 app.get('/', (req, res) => {
   res.send('Beche Daw is running succesfully!')
